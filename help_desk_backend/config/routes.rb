70ef7834ca5f573ae08668b0af4a766c5a392e93
Kate Larrick <katelarrick@umail.ucsb.edu>
2025-11-04 14:26:06 -0800

    Updated routes.rb with ExpertController actions

--- help_desk_backend/config/routes.rb
+++ help_desk_backend/config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
-  get "experts/queue"
-  get "experts/claim"
-  get "experts/unclaim"
-  get "experts/show"
-  get "experts/update"
-  get "experts/history"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   post '/auth/register', to: 'users#register'
+
+  get 'expert/queue', to: 'experts#queue'
+  post 'expert/conversations/:conversation_id/claim', to: 'experts#claim'
+  post 'expert/conversations/:conversation_id/unclaim', to: 'experts#unclaim'
+  get 'expert/profile', to: 'experts#show'
+  put 'expert/profile', to: 'experts#update'
+  get 'expert/assignments/history', to: 'experts#history'
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
